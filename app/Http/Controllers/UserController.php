11154976f491ef3f9475a49551dc4694e0438624
febrian <muhammaddwifebrian@gmail.com>
2026-03-11 17:42:17 +0700

    $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,17 +12,17 @@ class UserController extends Controller
     {
         //tambah data user dengan Eloquent Model
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
+            'level_id' => 2,    
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
             'password' => Hash::make('12345'),
-            'level_id' => 3
         ];
-        UserModel::insert($data); // tambahkan data ke tabel m_user
+        UserModel::create($data);
 
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
