4ffc348efeb4fc147873d96599b84ae97dd65dd7
febrian <muhammaddwifebrian@gmail.com>
2026-03-11 18:34:26 +0700

    findOr, findOrFail, firstOrFail

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
+use Dflydev\DotAccessData\Data;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -11,21 +12,31 @@ class UserController extends Controller
     public function index()
     {
         //tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,    
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
-
         // $data = [
-        //     'nama' => 'Pelanggan Pertama',
+        //     'level_id' => 2,    
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
         // ];
-        // UserModel::where('username', 'customer-1')->update($data); // update data user
+        // UserModel::create($data);
 
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        // // $data = [
+        // //     'nama' => 'Pelanggan Pertama',
+        // // ];
+        // // UserModel::where('username', 'customer-1')->update($data); // update data user
+
+        // // coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            // abort(404);
+        // });
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data'=>$user]);
     }
 }
