8170db2aab91c23d9c32661a05532ceaaa3cf6b2
febrian <muhammaddwifebrian@gmail.com>
2026-03-11 18:49:11 +0700

    Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,7 +36,8 @@ class UserController extends Controller
             // abort(404);
         // });
         // $user = UserModel::findOrFail(1);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
+        // $result = dd($user);
         return view('user', ['data'=>$user]);
     }
 }
